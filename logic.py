2400adb351c6faa0d13c83f711c58d1a61e151aa
unknown <s-touka99-mohamed@zewailcity.edu.eg>
2022-04-18 22:44:52 +0200

    fixed redundancy in the results table

--- logic.py
+++ logic.py
@@ -1,3 +1,4 @@
+import pymongo
 import helpers
 import app
 
@@ -6,7 +7,6 @@ def connect_thread():
     """
     wrapper around connect button function to make it run in a separate thread
     """
-
 
     print("thread started")
     confidence_thresh = 0.2  # minimum confidence threshold in top1 action to store it
@@ -18,6 +18,10 @@ def connect_thread():
     segments = helpers.video2segments(path, "video.mp4", segment_len=segment_len, stride=stride)
     # segments = 10
 
+    previous_action = " "
+    previous_beg = 0
+    previous_confidence = 0
+    clips = []
 
     for i in range(segments):  # process segment by segment
         print(f"progress: {int(i * 100 / segments)}%")  # print progress to terminal
@@ -25,8 +29,25 @@ def connect_thread():
         action = list(response.keys())[0]  # get top1 action
         confidence = list(response.values())[0]  # get confidence of top1 action
         if confidence >= confidence_thresh:  # only store action if confidence >= threshold
-            beg = i * stride  # beg time of segment = current segment index * stride length
-            end = beg + segment_len  # end time of segment = beg time + segment length
-            helpers.output(beg, end, action, confidence, i)
+
+            if action == previous_action:
+                last_row = app.results_data.find_one(
+                    {}, sort=[('_id', pymongo.DESCENDING)])
+                app.results_data.delete_one(last_row)
+                beg = previous_beg
+                end = i * stride + segment_len
+                confidence = (confidence + previous_confidence) / 2
+                clips.append(i)
+                helpers.output(beg, end, action, confidence, clips)
+
+            else:
+                beg = i * stride  # beg time of segment = current segment index * stride length
+                end = beg + segment_len  # end time of segment = beg time + segment length
+                clips = [i]
+                helpers.output(beg, end, action, confidence, i)
+
+            previous_action = action
+            previous_beg = beg
+            previous_confidence = confidence
 
     print("done!")
