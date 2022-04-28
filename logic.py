a5264168ad013402bd9b801b139ef1239d561b0c
mo-shaffei <s-mohamed_chaffei@zewailcity.edu.eg>
2022-04-28 23:27:01 +0200

    Cleaned code and added comments

--- logic.py
+++ logic.py
@@ -1,4 +1,3 @@
-import pymongo
 import helpers
 from models.recognizer import RecognizerModel
 from models.detection import DetectionModel
@@ -7,32 +6,28 @@ import time
 
 def connect_thread(app):
     """
-    wrapper around connect button function to make it run in a separate thread
+    Wrapper around connect button function to make it run in a separate thread
+    @param app: flask app (used to extract configurations for model and video)
+    @return: None
     """
 
-    print("thread started")
-    # minimum confidence threshold in top1 action to store it
-    print(app.config)
+    print("Inference thread started...")
+    # minimum confidence threshold for top1 action to store it
     confidence_thresh = app.config["model"]["recognition_threshold"]
     # stride length (in seconds) of temporal window which segments the input video
     stride = app.config["video"]["stride"]
     # segment length (in seconds) of each mini video segment
     segment_len = app.config["video"]["clip_length"]
     path = 'video/'  # store path of videos
-    video_name = app.config["video"]["video_name"]
+    video_name = app.config["video"]["video_name"]  # store name of video to process
     # segment the input video into multiple segments as required by segment_len and stride, return the resulting
     # number of segments
     segments = helpers.video2segments(path, video_name, segment_len=segment_len, stride=stride)
-    # segments = 10
 
-    previous_action = " "
-    previous_beg = 0
-    previous_confidence = 0
-    clips = []
-    bbox_threshold = app.config["model"]["bbox_threshold"]
-    visualize = app.config["model"]["visualize"]
-    device = app.config["model"]["device"]
-    if app.config["model"]["model_type"] == "recognition":
+    bbox_threshold = app.config["model"]["bbox_threshold"]  # store person bbox minimum confidence threshold
+    visualize = app.config["model"]["visualize"]  # store whether to visualize results or not
+    device = app.config["model"]["device"]  # store device used for inference
+    if app.config["model"]["model_type"] == "recognition":  # load appropriate model
         model_name = app.config["model"]["model_name"]
         model = RecognizerModel(model_name=model_name, person_bbox_threshold=bbox_threshold, device=device)
     else:
@@ -40,35 +35,19 @@ def connect_thread(app):
     start_time = time.time()
     for i in range(segments):  # process segment by segment
         print(f"progress: {int(i * 100 / segments)}%")  # print progress to terminal
-        # persons = helpers.inference(path + f"video_{i}.mp4", segment_len)  # perform inference on current segment
+        # perform inference on current segment
         persons = model.inference(path + f"video_{i}.mp4", visualize=visualize)
-        if not persons:
+        if not persons:  # if no persons detected skip this segment
             continue
 
-        for person in persons:
+        for person in persons:  # for each person detected
             action = list(person.keys())[0]  # get top1 action
             confidence = list(person.values())[0]  # get confidence of top1 action
             if confidence >= confidence_thresh:  # only store action if confidence >= threshold
-
-                # if action == previous_action:
-                #     last_row = app.results_data.find_one(
-                #         {}, sort=[('_id', pymongo.DESCENDING)])
-                #     app.results_data.delete_one(last_row)
-                #     beg = previous_beg
-                #     end = i * stride + segment_len
-                #     confidence = (confidence + previous_confidence) / 2
-                #     clips.append(i)
-                #     helpers.output(beg, end, action, confidence, clips)
-                #
-                # else:
                 beg = i * stride  # beg time of segment = current segment index * stride length
                 end = beg + segment_len  # end time of segment = beg time + segment length
-                # clips = [i]
                 helpers.output(beg, end, action, confidence, i)
 
-                # previous_action = action
-                # previous_beg = beg
-                # previous_confidence = confidence
     stop_time = time.time()
-    print("Done!")
+    print("Inference thread finished!")
     print(f"Time elapsed: {stop_time - start_time}")
