3528f950af9e39980061cf7f4386f63324eab26b
unknown <s-touka99-mohamed@zewailcity.edu.eg>
2022-05-11 14:46:57 +0200

    added comments and organized visualize.py

--- visualize.py
+++ visualize.py
@@ -6,9 +6,36 @@ import json
 from bson.json_util import dumps
 
 
-def plots(results, action='eating'):
+def plots(results, action='eating'):       #returns all plots of dashboard
 
-    def g1(action):
+    filtered_data = results.aggregate([     #filtered data for g2 and g4
+            {
+                "$group" : {
+                    "_id" :  {  "location": "$location", "action": "$action"} ,
+                    "count": { "$sum" : 1 } }
+            },
+
+            {
+                "$group" : {"_id" : "$_id.location", "actions": { 
+                "$push": { "action":"$_id.action", "count":"$count" }
+            } } 
+            }
+        ])
+        
+    list_data = list(filtered_data)
+    df = DataFrame(list_data)
+    x=[]; y=[]; z=[]; i=-1
+    for location in df._id:
+        i=i+1
+        for item in df.actions[i]:
+            x.append(item.pop('action'))
+            y.append(item.pop('count'))
+            z.append(location)
+
+        new_df=DataFrame(list(zip(x, y, z)),    #dataframe used in g2 and g4
+                columns=['action','count', 'location'])
+    ############################################################################
+    def g1(action):              #plots graph 1 (g1)
         
         filtered_data = results.aggregate([
         {
@@ -25,53 +52,24 @@ def plots(results, action='eating'):
 
         list_data = list(filtered_data)
         df = DataFrame(list_data)
-        df.head()
 
         fig = px.line(df, x="_id", y="count", labels={'x': 'start'}).update_layout(xaxis_title="start time")
         
         graphJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
-        #print(fig.data[0])
         
         return graphJSON
 
 
-    def g2(): 
+    def g2():                  #plots graph 2 (g2)
 
-        filtered_data = results.aggregate([
-            {
-                "$group" : {
-                    "_id" :  {  "location": "$location", "action": "$action"} ,
-                    "count": { "$sum" : 1 } }
-            },
-
-            {
-                "$group" : {"_id" : "$_id.location", "actions": { 
-                "$push": { "action":"$_id.action", "count":"$count" }
-            } } 
-            }
-        ])
-        
-        list_data = list(filtered_data)
-        df = DataFrame(list_data)
-        x=[]; y=[]; z=[]; i=-1
-        for location in df._id:
-            i=i+1
-            for item in df.actions[i]:
-                x.append(item.pop('action'))
-                y.append(item.pop('count'))
-                z.append(location)
-
-        new_df=DataFrame(list(zip(x, y, z)),
-                columns=['action','count', 'location'])
-
-        fig2 = px.bar(new_df, x="location", y="count", color='action',
+        fig2 = px.bar(new_df, x="location", y="count", color='action', #a bar plot of locations vs count of each action
         barmode='group')
 
         graph2JSON = json.dumps(fig2, cls=plotly.utils.PlotlyJSONEncoder)
         return graph2JSON
 
 
-    def g3():
+    def g3():                 #plots graph 3 (g3)
         counted_actions = results.aggregate([
             {
                 "$group" : {"_id" : "$action", "count": {"$sum" : 1}}  
@@ -81,37 +79,14 @@ def plots(results, action='eating'):
         list_data = list(counted_actions)
         df = DataFrame(list_data)
 
-        fig3 = px.pie(df, values='count', names='_id')
+        fig3 = px.pie(df, values='count', names='_id')   #pie chart of count of all actions
 
         graph3JSON = json.dumps(fig3, cls=plotly.utils.PlotlyJSONEncoder)
         return graph3JSON
 
-    def g4():
-        filtered_data = results.aggregate([
-                {
-                    "$group" : {
-                        "_id" :  {  "location": "$location", "action": "$action"} ,
-                        "count": { "$sum" : 1 } }
-                },
-
-                {
-                    "$group" : {"_id" : "$_id.location", "actions": { 
-                    "$push": { "action":"$_id.action", "count":"$count" }
-                } } 
-                }
-            ])
+    def g4():               #plots graph 4 (g4)
         
-        list_data = list(filtered_data)    
-        df = DataFrame(list_data)
-        x=[]; y=[]; z=[]; i=-1
-        for location in df._id:
-            i=i+1
-            for item in df.actions[i]:
-                x.append(item.pop('action'))
-                y.append(item.pop('count'))
-                z.append(location)
-
-        fig4 = go.Figure(data=go.Heatmap( {'z':y, 'x':x, 'y':z} ))
+        fig4 = go.Figure(data=go.Heatmap( {'z':y, 'x':x, 'y':z} ))  #a heatmap of actions vs locations
 
         graph4JSON = json.dumps(fig4, cls=plotly.utils.PlotlyJSONEncoder)
         return graph4JSON
