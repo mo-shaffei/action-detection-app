180d6f6b2ee9f8a1c99336915ca33d2977efa248
Fatma Moanes <fmoanesnoureldin@gmail.com>
2022-06-20 19:40:41 +0200

    Changed code structure

--- visualize.py
+++ visualize.py
@@ -6,23 +6,24 @@ import json
 from bson.json_util import dumps
 
 
-def plots(results, action='eating'):       #returns all plots and statistics of dashboard
+def helper(results):
+
     print("resultssssssss type is {}".format(type(results)))
     filtered_data = results.aggregate([     #filtered data for g2 and g4
-            {
-                "$group" : {
-                    "_id" :  {  "location": "$location", "action": "$action"} ,
-                    "count": { "$sum" : 1 } }
-            },
+        {
+            "$group" : {
+                "_id" :  {  "location": "$location", "action": "$action"} ,
+                "count": { "$sum" : 1 } }
+        },
 
-            {
-                "$group" : {"_id" : "$_id.location", "actions": { 
+        {
+            "$group" : {"_id" : "$_id.location", "actions": {
                 "$push": { "action":"$_id.action", "count":"$count" }
-            } } 
-            }
-        ])
-        
-    filtered_data = filtered_data.find()    
+            } }
+        }
+    ])
+
+    # filtered_data = filtered_data.find()
     list_data = list(filtered_data)
     df = DataFrame(list_data)
     x=[]; y=[]; z=[]; i=-1
@@ -33,92 +34,110 @@ def plots(results, action='eating'):       #returns all plots and statistics of 
             y.append(item.pop('count'))
             z.append(location)
 
-        new_df=DataFrame(list(zip(x, y, z)),    #dataframe used in g2 and g4
-                columns=['action','count', 'location'])
+        new_df = DataFrame(list(zip(x, y, z)),    #dataframe used in g2 and g4
+                           columns = ['action','count', 'location'])
     ############################################################################
 
     counted_actions = results.aggregate([       #counted actions needed in g3 and s1
-            {
-                "$group" : {"_id" : "$action", "count": {"$sum" : 1}}  
-            }
-        ])
-    list_counted_data = list(counted_actions)
-    ############################################################################    
-
-    def g1(action):              #plots graph 1 (g1)
-        
-        filtered_data = results.aggregate([
         {
-            "$match" : {
-                "action" : {
-                    "$eq" : action                               #filtering the entered action, then
-                } 
+            "$group" : {"_id" : "$action", "count": {"$sum" : 1}}
+        }
+    ])
+    list_counted_data = list(counted_actions)
+    ############################################################################
+
+    return filtered_data, new_df, list_counted_data, x, y, z
+
+
+def g1(results, action):  # plots graph 1 (g1)
+
+    filtered_data = results.aggregate([
+        {
+            "$match": {
+                "action": {
+                    "$eq": action  # filtering the entered action, then
+                }
             }
         },
         {
-            "$group" : {"_id" : "$start", "count": {"$sum" : 1}}  #counting the rows of this action for each start time
+            "$group": {"_id": "$start", "count": {"$sum": 1}}  # counting the rows of this action for each start time
         }
     ])
 
-        list_data = list(filtered_data)
-        df = DataFrame(list_data)
+    list_data = list(filtered_data)
+    df = DataFrame(list_data)
 
-        fig = px.bar(df, x="_id", y="count", labels={'x': 'start'}).update_layout(xaxis_title="start time")
-        
-        graphJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
-        
-        return graphJSON
+    fig = px.bar(df, x="_id", y="count", labels={'x': 'start'}).update_layout(xaxis_title="start time")
 
+    graphJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
 
-    def g2():                  #plots graph 2 (g2)
-
-        fig2 = px.bar(new_df, x="location", y="count", color='action', #a bar plot of locations vs count of each action
-        barmode='group')
-
-        graph2JSON = json.dumps(fig2, cls=plotly.utils.PlotlyJSONEncoder)
-        return graph2JSON
+    return graphJSON
 
 
-    def g3():                 #plots graph 3 (g3)
+def g2(new_df):  # plots graph 2 (g2)
 
-        df = DataFrame(list_counted_data)
+    fig2 = px.bar(new_df, x="location", y="count", color='action',  # a bar plot of locations vs count of each action
+                  barmode='group')
 
-        fig3 = px.pie(df, values='count', names='_id')   #pie chart of count of all actions
+    graph2JSON = json.dumps(fig2, cls=plotly.utils.PlotlyJSONEncoder)
+    return graph2JSON
 
-        graph3JSON = json.dumps(fig3, cls=plotly.utils.PlotlyJSONEncoder)
-        return graph3JSON
 
-    def g4():               #plots graph 4 (g4)
-        
-        fig4 = go.Figure(data=go.Heatmap( {'z':y, 'x':x, 'y':z} ))  #a heatmap of actions vs locations
+def g3(list_counted_data):  # plots graph 3 (g3)
 
-        graph4JSON = json.dumps(fig4, cls=plotly.utils.PlotlyJSONEncoder)
-        return graph4JSON
+    df = DataFrame(list_counted_data)
 
-    ###########################################################################################
+    fig3 = px.pie(df, values='count', names='_id')  # pie chart of count of all actions
+
+    graph3JSON = json.dumps(fig3, cls=plotly.utils.PlotlyJSONEncoder)
+    return graph3JSON
+
+
+def g4(x,y,z):  # plots graph 4 (g4)
+
+    fig4 = go.Figure(data=go.Heatmap({'z': y, 'x': x, 'y': z}))  # a heatmap of actions vs locations
+
+    graph4JSON = json.dumps(fig4, cls=plotly.utils.PlotlyJSONEncoder)
+    return graph4JSON
+
+
+###########################################################################################
+
+def s1(df_original):  # return statistics 1 (Top action)
+    top_action = df_original['action'].value_counts().idxmax()
+    return top_action
+
+
+def s2(df_original):  # return statistics 2 (Top location)
+    top_location = df_original['location'].value_counts().idxmax()
+    return top_location
+
+
+def s3(df_original):  # return statistics 3 (Top camera)
+    top_camera = df_original['camera_id'].value_counts().idxmax()
+    return top_camera
+
+
+def s4(df_original):  # return statistics 4 (camera that captured least actions)
+    min_camera = df_original['camera_id'].value_counts().idxmin()
+    return min_camera
+
+
+
+####### The below functions will be called in app.py
+
+
+def plot_all(results, action='eating'):
+    filtered_data, new_df, list_counted_data,x, y, z = helper(results)
+    return [g1(results,action), g2(new_df) , g3(list_counted_data), g4(x,y,z)]
+
+
+def statistics_all(results):
     results_data = list(results.find())
-    df_original = DataFrame(results_data)       
-
-    def s1():              #return statistics 1 (Top action)
-        top_action=df_original['action'].value_counts().idxmax() 
-        return top_action 
-
-    def s2():              #return statistics 2 (Top location)
-        top_location=df_original['location'].value_counts().idxmax()
-        return top_location
-
-    def s3():              #return statistics 3 (Top camera)
-        top_camera=df_original['camera_id'].value_counts().idxmax()  
-        return top_camera
-
-    def s4():              #return statistics 4 (camera that captured least actions)
-        min_camera=df_original['camera_id'].value_counts().idxmin()    
-        return min_camera
+    df_original = DataFrame(results_data)
+    return [s1(df_original), s2(df_original), s3(df_original), s4(df_original)]
 
 
-    return [g1(action), g2() , g3(), g4(), s1(), s2(), s3(), s4()]
-
-    
 #######################################################################################################
 
 
